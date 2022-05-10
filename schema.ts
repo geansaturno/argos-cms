43644b6dcd3cb4f673361b77ffdcdaec08eb8c46
Gean Saturno <geansaturno@gmail.com>
2022-05-09 21:45:20 -0300

    Argos cms

---
+++ schema.ts
@@ -0,0 +1,96 @@
+/*
+Welcome to the schema! The schema is the heart of Keystone.
+
+Here we define our 'lists', which will then be used both for the GraphQL
+API definition, our database tables, and our Admin UI layout.
+
+Some quick definitions to help out:
+A list: A definition of a collection of fields with a name. For the starter
+  we have `User`, `Post`, and `Tag` lists.
+A field: The individual bits of data on your list, each with its own type.
+  you can see some of the lists in what we use below.
+
+*/
+
+// Like the `config` function we use in keystone.ts, we use functions
+// for putting in our config so we get useful errors. With typescript,
+// we get these even before code runs.
+import { list } from '@keystone-6/core';
+
+// We're using some common fields in the starter. Check out https://keystonejs.com/docs/apis/fields#fields-api
+// for the full list of fields.
+import {
+  text,
+  relationship,
+  password,
+  timestamp,
+  select,
+  checkbox,
+} from '@keystone-6/core/fields';
+// The document field is a more complicated field, so it's in its own package
+// Keystone aims to have all the base field types, but you can make your own
+// custom ones.
+import { document } from '@keystone-6/fields-document';
+
+// We are using Typescript, and we want our types experience to be as strict as it can be.
+// By providing the Keystone generated `Lists` type to our lists object, we refine
+// our types to a stricter subset that is type-aware of other lists in our schema
+// that Typescript cannot easily infer.
+import { Lists } from '.keystone/types';
+
+// We have a users list, a blogs list, and tags for blog posts, so they can be filtered.
+// Each property on the exported object will become the name of a list (a.k.a. the `listKey`),
+// with the value being the definition of the list, including the fields.
+export const lists: Lists = {
+  // Here we define the user list.
+  User: list({
+    // Here are the fields that `User` will have. We want an email and password so they can log in
+    // a name so we can refer to them, and a way to connect users to posts.
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      email: text({
+        validation: { isRequired: true },
+        isIndexed: 'unique',
+        isFilterable: true,
+      }),
+      // The password field takes care of hiding details and hashing values
+      password: password({ validation: { isRequired: true } }),
+      // Relationships allow us to reference other lists. In this case,
+      // we want a user to have many posts, and we are saying that the user
+      // should be referencable by the 'author' field of posts.
+      // Make sure you read the docs to understand how they work: https://keystonejs.com/docs/guides/relationships#understanding-relationships
+      // posts: relationship({ ref: 'Post.author', many: true }),
+    },
+    // Here we can configure the Admin UI. We want to show a user's name and posts in the Admin UI
+    ui: {
+      listView: {
+        initialColumns: ['name'],
+      },
+    },
+  }),
+  Type: list({
+    fields: {
+      name: text(),
+      assets: relationship({ref: 'Asset.type', many: true}),
+    }
+  }),
+  Classification: list({
+    fields: {
+      name: text(),
+      assets: relationship({ref: 'Asset.classification', many: true})
+    }
+  }),
+  Asset: list({
+    fields: {
+      ticket: text(),
+      type: relationship({ref: 'Type.assets'}),
+      classification: relationship({ref: 'Classification.assets'}),
+      active: checkbox({defaultValue: true}),
+    },
+    ui: {
+      listView: {
+        initialColumns: ['ticket', 'type', 'classification', 'active'],
+      },
+    },
+  })
+};
